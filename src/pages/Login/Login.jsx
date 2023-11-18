9d84639e753d339f8b9f3d3567500cf708c27553
junny97 <pcohad12@gmail.com>
2023-11-18 17:10:09 +0900

    Style:로그인 페이지 UI 구현 & 입력 폼, 버튼 컴포넌트 적용#5

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,126 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from '../../components/common/Button/Button';
+import { AuthForm } from '../../components/common/Form/AuthForm';
+import KakaoIcon from '../../img/kakao-icon.svg';
+import GoogleIcon from '../../img/google-icon.svg';
 export default function Login() {
-  return <></>;
+  return (
+    <LoginWrap>
+      <LoginHeader>
+        <LoginTitle>
+          선곡 고민 끝, <br />
+          뮤딕에 오신 것을
+          <br /> 환영합니다!
+        </LoginTitle>
+        <LoginText>로그인하고 서비스를 이용해 주세요</LoginText>
+      </LoginHeader>
+      <LoginMain>
+        <LoginBtnBox>
+          <Button
+            text='카카오로 시작하기'
+            btnBgColor='#FBE101'
+            btnBorder='1px solid #FBE101'
+            btnColor={'var(--font-color)'}
+            imgSrc={KakaoIcon}
+            alt='카카오로 로그인하기 버튼'
+          />
+          <Button
+            text='구글로 시작하기'
+            btnBgColor='#FFF'
+            btnBorder='1px solid #DBDBDB'
+            btnColor={'var(--font-color)'}
+            imgSrc={GoogleIcon}
+            alt='구글로 로그인하기 버튼'
+          />
+        </LoginBtnBox>
+        <Span>또는</Span>
+        <AuthForm />
+        <NavUserInfo>
+          <NavUserInfoLink> 회원가입</NavUserInfoLink>
+          <NavUserInfoLink>아이디 · 비밀번호 찾기 </NavUserInfoLink>
+        </NavUserInfo>
+      </LoginMain>
+    </LoginWrap>
+  );
 }
+
+const LoginWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const LoginHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  top: 56px;
+  left: 16px;
+  line-height: 33px;
+`;
+
+const LoginTitle = styled.h1`
+  width: 149px;
+  font-size: var(--font-xl);
+  font-weight: var(--font-bold);
+`;
+
+const LoginText = styled.span`
+  width: 198px;
+  color: var(--sub-font--color);
+  font-size: var(--font-md);
+  line-height: 33px;
+`;
+const LoginMain = styled.main`
+  margin: 0 auto;
+  width: 328px;
+  position: relative;
+  text-align: center;
+  top: 132px;
+`;
+
+const LoginBtnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-bottom: 30px;
+`;
+
+const Span = styled.span`
+  position: relative;
+  display: block;
+  color: var(--font-color);
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    width: 131px;
+    height: 1px;
+
+    background-color: black;
+  }
+
+  &:before {
+    left: -4px;
+    background-color: var(--border-color);
+  }
+
+  &:after {
+    right: -4px;
+    background-color: var(--border-color);
+  }
+`;
+
+//footer
+const NavUserInfo = styled.nav`
+  width: 328px;
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const NavUserInfoLink = styled.span`
+  font-size: var(--font-sm);
+  cursor: pointer;
+`;
