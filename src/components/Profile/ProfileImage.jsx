86b08495d9616106b1ef9d4c8a70fdc39ad18382
Da-Youn <dayoun202@gmail.com>
2023-11-22 14:47:02 +0900

    Style: 프로필 페이지 UI 구현 #8
    - Myprofile/OtherProfile 페이지 컴포넌트 설정
    - 유저 정보 섹션 (ProfileSection)
    - 대표 플레이리스트 섹션 (MainPlayListSection)
    - 플레이리스트 섹션 (PlayListSection)
    - 이미지 파일 추가 (재생 아이콘, 레코드판, 프로필 데코)
    - 글로벌 스타일 수정 (추가로 사용되는 font-color, main 태그 스크롤 설정)

---
+++ src/components/Profile/ProfileImage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { CircleImage } from '../../components/common/Image/Image';
+
+import ProfileDecoImg from '../../img/profile-deco-img.svg';
+
+export default function ProfileImage() {
+  return (
+    <ProfileImgBox>
+      <CircleImage src='https://picsum.photos/200/300' alt='프로필 이미지' />
+    </ProfileImgBox>
+  );
+}
+
+const ProfileImgBox = styled.div`
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin-bottom: 16px;
+
+  &::after {
+    content: '';
+    background: url(${ProfileDecoImg}) no-repeat center/contain;
+    display: inline-block;
+    position: absolute;
+    width: 120px;
+    height: 60px;
+    top: -50%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+  }
+`;
