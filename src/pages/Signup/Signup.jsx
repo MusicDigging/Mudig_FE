24d499b15014f35a6a08df076264b5b8f8412a85
junny97 <pcohad12@gmail.com>
2023-11-22 18:08:36 +0900

    Style: SingupStyle import 축약#13

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,34 +1,21 @@
 import React from 'react';
 import { Button } from '../../components/common/Button/Button';
-import {
-  SignupWrap,
-  SignupHeader,
-  SignupTitle,
-  SignupText,
-  SignupMain,
-  SignupBtnBox,
-  Span,
-  NavLoign,
-  NavSpan,
-  LinkLogin,
-  Footer,
-  FooterSpan,
-} from './SignupStyle';
+import * as S from './SignupStyle';
 import KakaoIcon from '../../img/kakao-icon.svg';
 import GoogleIcon from '../../img/google-icon.svg';
 export default function Signup() {
   return (
-    <SignupWrap>
-      <SignupHeader>
-        <SignupTitle>
+    <S.SignupWrap>
+      <S.SignupHeader>
+        <S.SignupTitle>
           선곡 고민 끝, <br />
           뮤딕에 오신 것을
           <br /> 환영합니다!
-        </SignupTitle>
-        <SignupText>3초 가입으로 바로 시작해 보세요</SignupText>
-      </SignupHeader>
-      <SignupMain>
-        <SignupBtnBox>
+        </S.SignupTitle>
+        <S.SignupText>3초 가입으로 바로 시작해 보세요</S.SignupText>
+      </S.SignupHeader>
+      <S.SignupMain>
+        <S.SignupBtnBox>
           <Button
             text='카카오로 시작하기'
             btnBgColor='#FBE101'
@@ -45,24 +32,24 @@ export default function Signup() {
             imgSrc={GoogleIcon}
             alt='구글로 로그인하기 버튼'
           />
-        </SignupBtnBox>
-        <Span>또는</Span>
+        </S.SignupBtnBox>
+        <S.Span>또는</S.Span>
         <Button
           text='이메일로 시작하기'
           btnBorder='1px solid #DBDBDB'
           alt='구글로 로그인하기 버튼'
         />
-        <NavLoign>
-          <NavSpan>이미 계정이 있으신가요? </NavSpan>
-          <LinkLogin>로그인하기</LinkLogin>
-        </NavLoign>
-      </SignupMain>
-      <Footer>
-        <FooterSpan>
+        <S.NavLoign>
+          <S.NavSpan>이미 계정이 있으신가요? </S.NavSpan>
+          <S.LinkLogin>로그인하기</S.LinkLogin>
+        </S.NavLoign>
+      </S.SignupMain>
+      <S.Footer>
+        <S.FooterSpan>
           회원가입과 함께 <br />
           Muding의 약관에 동의하는 것으로 간주합니다.
-        </FooterSpan>
-      </Footer>
-    </SignupWrap>
+        </S.FooterSpan>
+      </S.Footer>
+    </S.SignupWrap>
   );
 }
