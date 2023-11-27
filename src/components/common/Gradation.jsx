ec955f7fa0ba673acd78785beca3174ee5a2d3e9
yeji <skql4128@naver.com>
2023-11-28 04:23:40 +0900

    style: 그라데이션 컴포넌트 #26

---
+++ src/components/common/Gradation.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Gradation() {
+  return (
+    <GradationWrap>
+      <PinkGradation></PinkGradation>
+      <PurpleGradation></PurpleGradation>
+      <SkyGradation></SkyGradation>
+    </GradationWrap>
+  );
+}
+const GradationWrap = styled.div`
+  width: 360px;
+  height: 100%;
+  position: relative;
+  z-index: 0;
+`;
+const PinkGradation = styled.span`
+  width: 573px;
+  height: 573px;
+  position: absolute;
+  border-radius: 573px;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    #ffc9da -4%,
+    rgba(252, 207, 221, 0) 100%
+  );
+  left: -158px;
+  top: -174px;
+  z-index: -1;
+`;
+const PurpleGradation = styled.span`
+  width: 761px;
+  height: 761px;
+  position: absolute;
+  border-radius: 761px;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    #de77d9 -4%,
+    rgba(229, 203, 246, 0) 100%
+  );
+  left: -121px;
+  top: 64px;
+  z-index: -1;
+`;
+const SkyGradation = styled.span`
+  width: 672px;
+  height: 672px;
+  position: absolute;
+  border-radius: 672px;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    #8ceaff -10%,
+    rgba(252, 207, 221, 0) 80.21%
+  );
+  left: -201px;
+  top: 247px;
+  z-index: -1;
+`;
