1bd886d9b92ce82d3cc595d93447c09fd83e6bb9
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-11-27 13:17:41 +0900

    Style: PlayListInfo 배치 틀어짐 수정 #33

--- src/components/PlaylistDetail/PlayListInfo.jsx
+++ src/components/PlaylistDetail/PlayListInfo.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import TestImg from '../../img/thumbnail-img.svg';
 import Mudig from '../../img/playlist-mudig-img.svg';
+
 import { useState } from 'react';
 export default function PlayListInfo() {
   const [moreInfoView, setMoreInfoView] = useState(false);
@@ -12,55 +13,48 @@ export default function PlayListInfo() {
   };
   return (
     <PlayListInfoWrap>
-      {moreInfoView && <ThumbnailBox />}
       <img src={TestImg} alt='썸네일' />
-      {moreInfoView ? (
-        <MoreInfoBox>
+      <InfoBox>
+        <h2>드라이브 할 때 듣기 좋은 K-POP</h2>
+        <div>
           <p>
             아래의 목록은 2010년대 K-POP 장르에 속하는 드라이브 할 때 적합한
             음악들입니다. 즐겁고 발랄한 느낌이 들며 상대방과 귀여운 분위기를
             공유할 수 있을 것입니다. 좋은 데이트를 즐기세요!
           </p>
-          <button onClick={handleCloseBtn}>닫기</button>
-        </MoreInfoBox>
-      ) : (
-        <InfoBox>
-          <h2>드라이브 할 때 듣기 좋은 K-POP</h2>
-          <div>
+          <button onClick={handleMoreBtn}>더보기</button>
+        </div>
+      </InfoBox>
+      {moreInfoView && (
+        <>
+          <ThumbnailBlurBox />
+          <MoreInfoBox>
             <p>
               아래의 목록은 2010년대 K-POP 장르에 속하는 드라이브 할 때 적합한
               음악들입니다. 즐겁고 발랄한 느낌이 들며 상대방과 귀여운 분위기를
               공유할 수 있을 것입니다. 좋은 데이트를 즐기세요!
             </p>
-            <button onClick={handleMoreBtn}>더보기</button>
-          </div>
-        </InfoBox>
+            <button onClick={handleCloseBtn}>닫기</button>
+          </MoreInfoBox>
+        </>
       )}
     </PlayListInfoWrap>
   );
 }
 const PlayListInfoWrap = styled.div`
   position: relative;
+  background-color: #c7c6c6;
   img {
-    position: relative;
-    transform: translate(50%, 0);
-    margin: 80px 0 25px;
+    transform: translate(50%, 20%);
+    margin: 40px 0 0;
     z-index: 2;
   }
-`;
-const ThumbnailBox = styled.div`
-  position: absolute;
-  width: 360px;
-  height: 342px;
-  background: rgba(15, 15, 16, 0.8);
-  z-index: 3;
 `;
 const InfoBox = styled.div`
   padding: 62px 16px 32px;
   background-color: #fff;
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
-  transform: translateY(-50%);
   h2 {
     font-size: var(--font-lg);
     font-weight: var(--font-semi-bold);
@@ -83,11 +77,19 @@ const InfoBox = styled.div`
     }
   }
 `;
+const ThumbnailBlurBox = styled.div`
+  position: absolute;
+  top: 0;
+  width: 360px;
+  height: 346px;
+  background: rgba(15, 15, 16, 0.8);
+  z-index: 3;
+`;
 const MoreInfoBox = styled.div`
   position: absolute;
   z-index: 3;
   display: flex;
-  transform: translateY(-30px);
+  bottom: 10px;
   flex-direction: column;
   padding: 16px;
   background-color: var(--playlist-info-bg-color);
