c5a9543999b343c03e9a37d6a1ec1d00bb7fb114
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-11-28 17:45:25 +0900

    Style: 플리 상세 더보기 버튼 활성화 #33

--- src/components/PlaylistDetail/DetailList.jsx
+++ src/components/PlaylistDetail/DetailList.jsx
@@ -3,23 +3,40 @@ import PlayListItem from '../common/PlayList/PlayListItem';
 import TestImg from '../../img/thumbnail-img.svg';
 import ExtendIcon from '../../img/extend-icon.svg';
 import styled from 'styled-components';
+import { useState } from 'react';
+
 export default function DetailList() {
+  // const [playlist, setPlaylist] = useState([]);
+  const [more, setMore] = useState(false);
+  /**
+   * 맨 처음에는 2개의 플리만 보여주고
+   * 더보기 버튼을 누르면 10개씩 추가로 보여준다.
+   * 마지막 플리까지 보여줬다면 화살표의 방향을 바꿔준다.
+   * 해당 버튼을 누르면 다시 처음의 2개의 플리만 보여준다.
+   */
+  const handleMore = () => {
+    setMore(!more);
+  };
   return (
     <DetailListWrap>
       <PlayList>
         <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
         <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        {/* <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
-        <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' /> */}
+        {more && (
+          <>
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+            <PlayListItem img={TestImg} title='ETA' info='NewJeans · 2:32' />
+          </>
+        )}
       </PlayList>
-      <ExtendBtn>
-        <img src={ExtendIcon} alt='플레이리스트 확장' />
+      <ExtendBtn onClick={handleMore} more={more}>
+        <img src={ExtendIcon} alt='플레이리스트 접기' />
       </ExtendBtn>
     </DetailListWrap>
   );
@@ -30,4 +47,8 @@ const DetailListWrap = styled.div`
   padding: 0 16px;
   overflow-y: scroll;
 `;
-const ExtendBtn = styled.button``;
+const ExtendBtn = styled.button`
+  img {
+    transform: rotate(${({ more }) => (more ? '180deg' : '0deg')});
+  }
+`;
