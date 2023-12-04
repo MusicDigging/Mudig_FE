9c742d6e460499931939772d2198fc836610df7d
junny97 <pcohad12@gmail.com>
2023-12-04 20:54:04 +0900

    Feat: Layout NavBar 조건부처리

--- src/components/common/Layout/Layout.jsx
+++ src/components/common/Layout/Layout.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 import NavBar from '../NavBar/NavBar';
 
 export default function Layout({ children }) {
+  const location = useLocation();
+
+  const pathsWithoutNavBar = [
+    '/main',
+    '/search',
+    '/playlist',
+    '/user/profile',
+    '/randomplay',
+  ];
+
+  const isNavBarHidden = pathsWithoutNavBar.includes(location.pathname);
+
   return (
     <>
       <Header />
+      {isNavBarHidden && <NavBar />}
       {children}
-      <NavBar />
     </>
   );
 }
