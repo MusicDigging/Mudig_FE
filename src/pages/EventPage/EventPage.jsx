ce285789013f0ccb3bedb94f1950ed7062a4d3e7
yeji <skql4128@naver.com>
2023-12-06 22:50:36 +0900

    feat: Event Page 기능 구현 #60

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './EventPageStyle';
 import { useNavigate } from 'react-router-dom';
+import { privateInstance } from '../../library/apis/axiosInstance';
 
 export default function EventPage() {
-  // 닫기 버튼은 홈으로 이동
-  const navigate = useNavigate(); // useNavigate 훅 사용
-
-  // 닫기 버튼 클릭 핸들러
+  const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
   const handleClose = () => {
     navigate('/');
   };
 
-  // 생성 버튼은 플리 생성 로딩에서 플리 생성 결과로 이동
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    if (newValue.length <= 100) {
+      // 100자 이내의 입력만 허용
+      setInputValue(newValue);
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await privateInstance.post('/playlist/event/', {
+        situations: inputValue,
+      });
+
+      console.log('response.data:', response.data);
+
+      if (response.status === 200) {
+        const { message } = response.data;
+        console.log(message);
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('전송 실패', error);
+    }
+  };
 
   return (
     <S.EventPageWrap>
       <S.CloseButton onClick={handleClose} />
       <S.QuestionBox>
         <p>당신의 하루는 어떠셨나요?</p>
-        <input placeholder='답변 입력하기' type='text'></input>
-        <S.SubmitButton>생성</S.SubmitButton>
+        <input
+          id='answer'
+          placeholder='답변 입력하기'
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+        ></input>
+        <div>{inputValue.length}/100</div>
+        <S.SubmitButton onClick={handleSubmit}>생성</S.SubmitButton>
       </S.QuestionBox>
     </S.EventPageWrap>
   );
