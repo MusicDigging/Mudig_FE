3a43101df815f34ab878257b5edcff7b83a533d2
junny97 <pcohad12@gmail.com>
2023-12-09 22:36:15 +0900

    소셜 로그인/회원가입에 필요한 api 설청#73

--- src/library/apis/api.js
+++ src/library/apis/api.js
@@ -8,3 +8,50 @@ export const loginUser = async (data) => {
 
   return response.data;
 };
+
+export const getKakaoInfo = async () => {
+  try {
+    const response = await axiosInstance.get('/user/login/kakao');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postUserCode = async (code, social) => {
+  try {
+    const response = await axiosInstance.post(
+      `/user/login/${social}/callback/`,
+
+      {
+        code: code,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postUserProfile = async (formData, userType) => {
+  //userType 따라 소셜 회원가입/ 자체 회원가입 나누어  post 요청 보내기
+  const apiUrl = userType === 'social' ? '/user/socialjoin/' : '/user/join/';
+  const response = await axiosInstance.post(apiUrl, formData);
+  return response.data;
+};
+
+export const getGoogleInfo = async () => {
+  try {
+    const response = await axiosInstance.get('/user/login/google/');
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
