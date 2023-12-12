a4c85403cf73192f832aff4b147b8103d657e1cd
junny97 <pcohad12@gmail.com>
2023-12-12 23:14:24 +0900

    test: 랜덤뮤비 받아오기 test 코드 작성#78

--- src/pages/RandomMusic/RandomMusic.jsx
+++ src/pages/RandomMusic/RandomMusic.jsx
@@ -1,46 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import VideoPlayer from '../../components/RandomMv/VideoPlayer';
 import VideoInfo from '../../components/RandomMv/VideoInfo';
 import MainHeader from '../../components/common/Header/MainHeader';
+import axios from 'axios';
+
+import { useMutation } from 'react-query';
+import { userInfoAtom } from '../../library/atom';
+import { useRecoilValue } from 'recoil';
 export default function RandomMusic() {
+  const token = useRecoilValue(userInfoAtom).token.access;
+  const [videoData, setVideoData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          'https://api.mudig.co.kr/playlist/random-mv/',
+          {
+            already_musiclist: [null],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+
+        if (response.status === 200) {
+          setVideoData(response.data);
+        }
+      } catch (error) {
+        console.error('데이터 받아오기 실패 :', error);
+      }
+
+      console.log(videoData);
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <MainHeader />
       <PlayerWrap>
-        <PlayerBox>
-          <VideoPlayer
-            url={
-              'https://www.youtube.com/embed/jIG4AaIy-5k?si=uaOtD3J9A6ul7_ix'
-            }
-          />
-          <VideoInfo
-            title={'Get A Guitar'}
-            views={`${'RIIZE'} · 조회수 ${428}만회 `}
-          />
-        </PlayerBox>
-        <PlayerBox>
-          <VideoPlayer
-            url={
-              'https://www.youtube.com/embed/iTWYa0t5COk?si=Ie3Aly8haYDOIMkM'
-            }
-          />
-          <VideoInfo
-            title={'Sleep Well'}
-            views={`${'d4dv'} · 조회수 ${1125}만회 `}
-          />
-        </PlayerBox>
-        <PlayerBox>
-          <VideoPlayer
-            url={
-              'https://www.youtube.com/embed/hLvWy2b857I?si=5PFepg7TfnH6H8we'
-            }
-          />
-          <VideoInfo
-            title={'Perfect Night'}
-            views={`${'LE SSERAFIM (르세라핌)'} · 조회수 ${3959}만회 `}
-          />
-        </PlayerBox>
+        {videoData?.map((video, index) => (
+          <PlayerBox id={video.id} key={index}>
+            <VideoPlayer url={video.information} />
+            <VideoInfo title={video.song} views={`${video.singer}`} />
+          </PlayerBox>
+        ))}
       </PlayerWrap>
     </>
   );
