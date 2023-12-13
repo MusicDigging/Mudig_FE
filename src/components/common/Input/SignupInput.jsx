4a34530b2be343cc506b86aa4a1ea2a5876beb81
junny97 <pcohad12@gmail.com>
2023-12-13 15:06:16 +0900

    Fix: 비밀번호 보기/ 숨기기 기능 버그 해결 재설정#28

--- src/components/common/Input/SignupInput.jsx
+++ src/components/common/Input/SignupInput.jsx
@@ -40,7 +40,7 @@ export const SignupInput = (props) => {
         {showTimeText && <TimeText>05:00</TimeText>}
 
         <EyeIcon onClick={toggleShowPassword}>
-          {type === 'password' && (
+          {type === 'password' && name !== 'confirmPassword' && (
             <img
               src={showPassword ? showEye : hideEye}
               alt={showPassword ? '비밀번호 표시' : '비밀번호 숨기기'}
--- src/pages/UserInfo/ChangePwForm.jsx
+++ src/pages/UserInfo/ChangePwForm.jsx
@@ -4,32 +4,74 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { Button } from '../../components/common/Button/Button';
 import { SignupInput } from '../../components/common/Input/SignupInput';
 import usePasswordToggle from '../../hooks/ussPasswordToggle';
+import { userInfoAtom } from '../../library/atom';
+import { useRecoilValue } from 'recoil';
+import { useMutation } from 'react-query';
+import Swal from 'sweetalert2';
+
+import axios from 'axios';
 export default function ChangePwForm() {
+  const userEmail = useRecoilValue(userInfoAtom).email;
+  const token = useRecoilValue(userInfoAtom).token.access;
   const pawwrodRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$/;
   const methods = useForm({
     defaultValues: {
-      email: 'mudic@naver.com',
+      email: userEmail,
     },
 
     mode: 'onBlur',
   });
-  const { formState } = methods;
+
+  const { formState, setError, watch } = methods;
   const { isValid } = formState;
-  const onSubmit = (data) => console.log(data);
+
+  const { mutate } = useMutation(async (data) => {
+    const { password, newPassword } = data;
+    try {
+      const response = await axios.put(
+        'https://api.mudig.co.kr/user/changepassword/',
+        {
+          old_password: password,
+          new_password: newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      Swal.fire({
+        icon: 'success',
+        title: '비밀번호 변경 완료',
+        text: '비밀번호가 성공적으로로 변경되었습니다!',
+      });
+      console.log(response.data);
+    } catch (error) {
+      setError('password', { message: '현재 비밀번호가 일치하지 않습니다.' });
+      console.error('비밀번호 변경 실패', error);
+    }
+  });
+
+  const onSubmit = (data) => {
+    mutate(data);
+  };
   const { toggleShowPassword, showPassword } = usePasswordToggle();
   return (
     <FormProvider {...methods}>
       <FormWrap onSubmit={methods.handleSubmit(onSubmit)}>
         <InputBox>
-          <SignupInput placeholder='아이디' type='text' name='email' />
+          <Label>이메일 </Label>
+          <SignupInput placeholder='이메일' type='text' name='email' />
 
+          <Label>현재 비밀번호</Label>
           <SignupInput
             validation={{
               pattern: {
                 value: pawwrodRegex, //기능개발할땐 기존 유저 비밀번호를 검사해야 됨
                 message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
               },
-              required: '현재 비밀번호가 기억나지 않으세요?',
+
+              required: '현재 사용중인 비밀번호를 입력하세요.',
             }}
             placeholder='현재 비밀번호'
             type='password'
@@ -37,13 +79,14 @@ export default function ChangePwForm() {
             showPassword={showPassword.password}
             toggleShowPassword={() => toggleShowPassword('password')}
           />
-
+          <Label>새 비밀번호</Label>
           <SignupInput
             validation={{
               pattern: {
                 value: pawwrodRegex,
                 message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
               },
+
               required: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
             }}
             placeholder='새 비밀번호'
@@ -52,11 +95,20 @@ export default function ChangePwForm() {
             showPassword={showPassword.newPassword}
             toggleShowPassword={() => toggleShowPassword('newPassword')}
           />
+          <Label>새 비밀번호 확인</Label>
           <SignupInput
             validation={{
               pattern: {
                 value: pawwrodRegex,
                 message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+              },
+              validate: {
+                comfirmPw: (fieldValue) => {
+                  return (
+                    fieldValue == watch('newPassword') ||
+                    '비밀번호 확인이 일치하지 않습니다.'
+                  );
+                },
               },
               required: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
             }}
@@ -80,7 +132,9 @@ export default function ChangePwForm() {
   );
 }
 
-const FormWrap = styled.form``;
+const FormWrap = styled.form`
+  height: 100%;
+`;
 
 const InputBox = styled.div`
   display: flex;
@@ -92,6 +146,11 @@ const InputBox = styled.div`
 
 const ButtonBox = styled.div`
   position: absolute;
-  top: 680px;
+  top: 755px;
   padding: 0 16px;
+`;
+
+const Label = styled.label`
+  padding-bottom: 8px;
+  font-size: var(--font-md);
 `;
