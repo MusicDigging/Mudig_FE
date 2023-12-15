a8652162192366d3b91992811f6ccb44b449653a
junny97 <pcohad12@gmail.com>
2023-12-15 14:44:21 +0900

    Feat: 회원탈퇴 페이지 기능구현#85

--- src/hooks/queries/useUserInfo.jsx
+++ src/hooks/queries/useUserInfo.jsx
@@ -1,6 +1,7 @@
 import { useMutation } from 'react-query';
 import { privateInstance } from '../../library/apis/axiosInstance';
 
+//비밀번호 변경
 export const useChangePassword = () => {
   const changePassword = async (data) => {
     const response = await privateInstance.put('/user/changepassword/', {
@@ -11,5 +12,17 @@ export const useChangePassword = () => {
     return response.data;
   };
   const mutation = useMutation(changePassword);
+  return mutation;
+};
+
+//회원탈퇴
+export const useUserResign = () => {
+  const userResign = async (password) => {
+    const response = await privateInstance.delete('/user/withdrawal/', {
+      data: { password: password },
+    });
+    return response.data;
+  };
+  const mutation = useMutation(userResign);
   return mutation;
 };
