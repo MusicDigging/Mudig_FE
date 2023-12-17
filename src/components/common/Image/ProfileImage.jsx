3056a80e514f904013e515d5f6df99a1bda993aa
Da-Youn <dayoun202@gmail.com>
2023-12-17 23:51:02 +0900

    Fix: image값 undefined 시 오류 수정 #76

--- src/components/common/Image/ProfileImage.jsx
+++ src/components/common/Image/ProfileImage.jsx
@@ -8,7 +8,7 @@ import ProfileDecoImg from '../../../img/profile-deco-img.svg';
 export default function ProfileImage(props) {
   const { src, alt, children } = props;
   const filterdSrc =
-    src.startsWith('karlo') || src.startsWith('profile')
+    src?.startsWith('karlo') || src?.startsWith('profile')
       ? `https://mudigbucket.s3.ap-northeast-2.amazonaws.com/${src}`
       : src;
 
