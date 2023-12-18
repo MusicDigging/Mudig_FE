41ad052ef50c9f9c2390adf5f32fef9ba9e75a24
Da-Youn <dayoun202@gmail.com>
2023-12-18 23:07:27 +0900

    Fix: id 고정값 원래대로 복원 #95

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -18,10 +18,8 @@ import * as S from './ProfileStyle';
 export default function Profile() {
   const location = useLocation();
   const state = location.state;
-  const my_id = 67; //useRecoilValue(userInfoAtom).id;
-  const user_id = 67; //state?.id || my_id;
-  const { data: profileData, isLoading: profileLoading } =
-    useGetProfile(user_id);
+  const my_id = useRecoilValue(userInfoAtom).id;
+  const user_id = state?.id || my_id;
   const { data: followingData, isLoading: followingLoading } =
     useGetFollowing(user_id);
   const { data: followerData, isLoading: followerLoading } =
