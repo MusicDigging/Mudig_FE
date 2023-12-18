6a5d67b4d4ed50c3a0e540dba982a70ad71e9e5c
yeji <skql4128@naver.com>
2023-12-18 23:15:34 +0900

    Feat: 유저 데이터 recoil로 가져오기 #61

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,45 +1,36 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { userInfoAtom } from '../../library/atom'; // 'userInfoAtom'의 실제 경로를 사용하세요
 import * as S from './HomeStyle';
 import MainHeader from '../../components/common/Header/MainHeader';
 import MyPlayListTable from '../../components/Home/MyPlayListTable';
 import PlayListTable from '../../components/Home/PlayListTable';
 import useGetHome from '../../hooks/queries/useHome';
-// import { privateInstance } from '../../library/apis/axiosInstance';
 import Loading from '../../components/Loading/Loading';
 import NotFound from '../NotFound/NotFound';
-import { useRecoilState } from 'recoil';
-import { userInfoAtom } from '../../library/atom';
 
 export default function Home() {
-  // privateInstance
-  //   .get('/playlist/')
-  //   .then((response) => {
-  //     console.log(response.data); // 여기에서 응답 데이터를 처리합니다.
-  //   })
-  //   .catch((error) => {
-  //     console.error('API 요청 중 오류 발생:', error);
-  //   });
   const { data, isLoading, error } = useGetHome();
-  // const [userInfo, setUserInfo] = useRecoilState(userInfoAtom);
-  // const userAccountname = userInfo.accountname;
-  // console.log(userInfo);
+  const userInfo = useRecoilValue(userInfoAtom); // 유저 정보 가져오기
+
   if (isLoading) return <Loading isLoading={isLoading} />;
   if (error) return <NotFound />;
   const { liked_playlist, my_playlist, playlist_all, recommend_pli } = data;
-  // console.log({ liked_playlist, my_playlist, playlist_all, recommend_pli });
+
   return (
     <S.HomeWrap>
       <MainHeader />
       <S.HomeSection>
-        <h2 id='bold'>고길동2님을 위한 플레이리스트</h2>
+        {/* 유저 이름으로 대체 */}
+        <h2 id='bold'>{userInfo.name}님을 위한 플레이리스트</h2>
         <PlayListTable
           liSize={{ width: '152px' }}
-          playlistData={liked_playlist}
+          playlistData={recommend_pli}
         />
         <h2>지금 핫한🔥 플레이리스트</h2>
         <PlayListTable
           liSize={{ width: '118px' }}
-          playlistData={recommend_pli}
+          playlistData={liked_playlist}
         />
         <h2>내가 생성한 플레이리스트</h2>
         <MyPlayListTable playlistData={my_playlist} />
