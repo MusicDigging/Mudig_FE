144edb5e7019d75f62b4d43d4dba2f47f24ecdb0
yeji <skql4128@naver.com>
2023-12-21 02:55:05 +0900

    Fix: refetch, queryClient 설정 #113

--- src/hooks/queries/useFollow.jsx
+++ src/hooks/queries/useFollow.jsx
@@ -1,14 +1,32 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { privateInstance } from '../../library/apis/axiosInstance';
 
 export const usePostFollow = () => {
-  return useMutation((user_id) =>
-    privateInstance.post(`/user/${user_id}/follow`),
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (user_id) => privateInstance.post(`/user/${user_id}/follow/`),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('get-following');
+      },
+    },
   );
 };
 
 export const useDelFollow = () => {
-  return useMutation((user_id) =>
-    privateInstance.delete(`/user/${user_id}/unfollow`),
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (user_id) => privateInstance.delete(`/user/${user_id}/unfollow/`),
+    {
+      onSuccess: () => {
+        // 팔로잉 목록 쿼리 갱신
+        queryClient.invalidateQueries('get-following');
+
+        // 필요하다면, 팔로워 목록 쿼리도 갱신할 수 있습니다.
+        // queryClient.invalidateQueries('get-follower');
+      },
+    },
   );
 };
