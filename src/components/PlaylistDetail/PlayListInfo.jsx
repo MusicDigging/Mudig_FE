030218dce5d58c1934ca142003b16c2b381b563e
Da-Youn <dayoun202@gmail.com>
2023-12-25 11:37:21 +0900

    Style: 블러 가장자리 안보이게 하기 #132

--- src/components/PlaylistDetail/PlayListInfo.jsx
+++ src/components/PlaylistDetail/PlayListInfo.jsx
@@ -110,7 +110,8 @@ const PlayListInfoWrap = styled.section`
     -moz-filter: blur(10px);
     -o-filter: blur(10px);
     -ms-filter: blur(10px);
-    filter: blur(10px);
+    filter: blur(8px);
+    transform: scale(1.05);
     z-index: -1;
   }
 `;
