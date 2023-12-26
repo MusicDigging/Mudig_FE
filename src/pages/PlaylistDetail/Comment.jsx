05162a3118dc16422d678941d089822e7e72bc4f
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-12-26 23:25:14 +0900

    Style: esLint 에러

--- src/pages/PlaylistDetail/Comment.jsx
+++ src/pages/PlaylistDetail/Comment.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 export default function Comment() {
-  return (
-    <CommentWrap>
-    </CommentWrap>
-  );
+  return <CommentWrap></CommentWrap>;
 }
 
 const CommentWrap = styled.div`
