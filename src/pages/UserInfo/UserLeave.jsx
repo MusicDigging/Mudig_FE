6f113d373ff08e5e158aab0fe22be26e350e6e90
junny97 <pcohad12@gmail.com>
2023-12-29 16:20:42 +0900

    Style: 비밀번호 변경, 회원탈퇴 페이지 뒤로가기 버튼 추가#148

--- src/pages/UserInfo/UserLeave.jsx
+++ src/pages/UserInfo/UserLeave.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import UserLeaveForm from './UserLeaveForm';
+import BackBtnIcon from '../../img/left-arrow-Icon.svg';
+import { useNavigate } from 'react-router-dom';
+
 export default function UserLeave() {
+  const navigate = useNavigate();
   return (
     <UserLeavewWrap>
+      <BackBtn>
+        <button onClick={() => navigate(-1)}>
+          <img src={BackBtnIcon} alt='뒤로가기 버튼' />
+        </button>
+      </BackBtn>
       <UserLeaveTitle>
         본인확인을 위해
         <br />
@@ -21,11 +30,18 @@ const UserLeavewWrap = styled.div`
   height: 100%;
 `;
 
+const BackBtn = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 12px;
+`;
+
 const UserLeaveTitle = styled.h1`
   font-size: var(--font-xl);
   font-weight: var(--font-bold);
   line-height: 33px;
   position: relative;
   top: 56px;
+
   left: 16px;
 `;
