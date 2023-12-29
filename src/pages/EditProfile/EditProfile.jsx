90994f9f8547c1c667ec8ddf19417a53a8df21de
Da-Youn <dayoun202@gmail.com>
2023-12-29 21:08:47 +0900

    Feat: 프로필 수정 페이지 토스트 메시지 설정 #164

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useRef } from 'react';
+import { useRecoilState } from 'recoil';
 import { useNavigate, useLocation } from 'react-router';
 
+import { toastAtom } from '../../library/atom';
 import { useEditProfile } from '../../hooks/queries/useProfile';
 
+import Toast from '../../components/common/Toast';
 import ProfileInput from '../../components/common/Input/ProfileInput';
 import SetRepPlaylist from '../../components/EditProfile/SetRepPlaylist';
 import SetProfileImage from '../../components/EditProfile/SetProfileImage';
@@ -18,7 +21,9 @@ export default function EditProfile() {
   const { playlist, profile } = data;
   const [genre, setGenre] = useState(profile?.genre.split(',') || []);
   const [uploadImg, setUploadImg] = useState(null);
+  const [toast, setToast] = useRecoilState(toastAtom);
   const [repPlaylist, setRepPlaylist] = useState(profile.rep_playlist);
+
   const [previewImg, setPreviewImg] = useState(
     profile.image ||
       'https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg',
@@ -48,7 +53,12 @@ export default function EditProfile() {
 
   return (
     <S.EditProfileWrap>
+      {toast && (
+        <S.ToastBox>
+          <Toast setToast={setToast} text={toast.content} type={toast.type} />
+        </S.ToastBox>
       <S.MoveBackBtn to='/user/profile/my'>
+      <S.MoveBackBtn onClick={handleMoveBackBtnClick}>
         <img src={ArrowIcon} alt='' />
       </S.MoveBackBtn>
       <S.EditProfileBox>
