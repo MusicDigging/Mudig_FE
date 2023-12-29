8bfca8dba380025cdb314723994be024e58b86db
junny97 <pcohad12@gmail.com>
2023-12-29 21:43:02 +0900

    Style: 이메일, 비밀번호 유효성검사 메시지 수정#166

--- src/pages/UserInfo/ChangePwForm.jsx
+++ src/pages/UserInfo/ChangePwForm.jsx
@@ -4,8 +4,8 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { Button } from '../../components/common/Button/Button';
 import { SignupInput } from '../../components/common/Input/SignupInput';
 import usePasswordToggle from '../../hooks/ussPasswordToggle';
-import { userInfoAtom } from '../../library/atom';
-import { useRecoilValue } from 'recoil';
+import { isLoginAtom, toastAtom, userInfoAtom } from '../../library/atom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useChangePassword } from '../../hooks/queries/useUserInfo';
 
 import {
@@ -18,6 +18,9 @@ export default function ChangePwForm() {
   const navigate = useNavigate();
   const userEmail = useRecoilValue(userInfoAtom).email;
   const { mutate: changePassword } = useChangePassword();
+  const setIsLogin = useSetRecoilState(isLoginAtom);
+  const setUserInfo = useSetRecoilState(userInfoAtom);
+  const setToast = useSetRecoilState(toastAtom);
   const pawwrodRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,16}$/;
   const methods = useForm({
     defaultValues: {
@@ -33,9 +36,15 @@ export default function ChangePwForm() {
   const handlePasswordSubmit = (data) => {
     changePassword(data, {
       onSuccess: (data) => {
-        showCustomModal(changePassworrdModal);
+        setToast({
+          content: '비밀번호 변경이 완료되었습니다.',
+          type: 'success',
+        });
+        setIsLogin(false);
+        setUserInfo({});
+        localStorage.removeItem('token');
+        localStorage.removeItem('refreshToken');
         navigate('/login');
-        console.log(data);
       },
       onError: (error) => {
         console.error('비밀번호 변경 실패', error);
@@ -56,7 +65,8 @@ export default function ChangePwForm() {
             validation={{
               pattern: {
                 value: pawwrodRegex,
-                message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+                message:
+                  '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
               },
 
               required: '현재 사용중인 비밀번호를 입력하세요.',
@@ -73,7 +83,8 @@ export default function ChangePwForm() {
             validation={{
               pattern: {
                 value: pawwrodRegex,
-                message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+                message:
+                  '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
               },
               validate: {
                 comfirmPw: (fieldValue) => {
@@ -86,7 +97,8 @@ export default function ChangePwForm() {
                   return null;
                 },
               },
-              required: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+              required:
+                '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
             }}
             placeholder='새 비밀번호'
             type='password'
@@ -99,7 +111,8 @@ export default function ChangePwForm() {
             validation={{
               pattern: {
                 value: pawwrodRegex,
-                message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+                message:
+                  '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
               },
               validate: {
                 comfirmPw: (fieldValue) => {
@@ -110,7 +123,8 @@ export default function ChangePwForm() {
                 },
               },
 
-              required: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+              required:
+                '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
             }}
             placeholder='새 비밀번호 확인'
             type='password'
--- src/pages/UserInfo/UserLeaveForm.jsx
+++ src/pages/UserInfo/UserLeaveForm.jsx
@@ -73,7 +73,8 @@ export default function UserLeaveForm() {
               validation={{
                 pattern: {
                   value: pawwrodRegex, //기능개발할땐 기존 유저 비밀번호를 검사해야 됨
-                  message: 'X 8~16자 영문 대 소문자, 숫자를 사용하세요.',
+                  message:
+                    '비밀번호는 8~16자 영문 대 소문자, 숫자를 조합해서 사용하세요.',
                 },
                 required: '현재 사용중인 비밀번호를 입력하세요.',
               }}
