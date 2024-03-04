0b26e80c7791ddf0bab43be9cdad72e1cef41730
junny97 <pcohad12@gmail.com>
2024-03-04 21:27:10 +0900

    Refactor: 로그인 스타일 분리

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,11 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import { Button } from '../../components/common/Button/Button';
 import { AuthForm } from '../../components/common/Form/AuthForm';
 import KakaoIcon from '../../img/kakao-icon.svg';
 import GoogleIcon from '../../img/google-icon.svg';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { isLoginAtom, signUpInfoAtom, userInfoAtom } from '../../library/atom';
-import {
-  getKakaoInfo,
-  getGoogleInfo,
-  postUserCode,
-} from '../../library/apis/api';
-
+import * as S from './LoginStlye';
+import { useNavigate } from 'react-router-dom';
+import { getKakaoInfo, getGoogleInfo } from '../../library/apis/api';
 import { useSocialLogin } from '../../hooks/useSocialLogin';
 export default function Login() {
   const navigate = useNavigate();
@@ -20,17 +14,17 @@ export default function Login() {
   const googleLoginHandler = useSocialLogin(getGoogleInfo, 'google');
 
   return (
-    <LoginWrap>
-      <LoginHeader>
-        <LoginTitle>
+    <S.LoginWrap>
+      <S.LoginHeader>
+        <S.LoginTitle>
           선곡 고민 끝, <br />
           뮤딕에 오신 것을
           <br /> 환영합니다!
-        </LoginTitle>
-        <LoginText>로그인하고 서비스를 이용해 주세요</LoginText>
-      </LoginHeader>
-      <LoginMain>
-        <LoginBtnBox>
+        </S.LoginTitle>
+        <S.LoginText>로그인하고 서비스를 이용해 주세요</S.LoginText>
+      </S.LoginHeader>
+      <S.LoginMain>
+        <S.LoginBtnBox>
           <Button
             text='카카오로 시작하기'
             btnBgColor='#FBE101'
@@ -49,77 +43,18 @@ export default function Login() {
             onClick={googleLoginHandler}
             alt='구글로 로그인하기 버튼'
           />
-        </LoginBtnBox>
-        <Span>또는</Span>
+        </S.LoginBtnBox>
+        <S.Span>또는</S.Span>
         <AuthForm />
-        <NavUserInfo>
-          <NavUserInfoLink onClick={() => navigate('/register')}>
+        <S.NavUserInfo>
+          <S.NavUserInfoLink onClick={() => navigate('/register')}>
             회원가입
-          </NavUserInfoLink>
-          <NavUserInfoLink onClick={() => navigate('/password/find')}>
+          </S.NavUserInfoLink>
+          <S.NavUserInfoLink onClick={() => navigate('/password/find')}>
             비밀번호 찾기
-          </NavUserInfoLink>
-        </NavUserInfo>
-      </LoginMain>
-    </LoginWrap>
+          </S.NavUserInfoLink>
+        </S.NavUserInfo>
+      </S.LoginMain>
+    </S.LoginWrap>
   );
 }
-
-const LoginWrap = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 0 16px;
-`;
-
-const LoginHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  top: 56px;
-
-  line-height: 33px;
-`;
-
-const LoginTitle = styled.h1`
-  font-size: var(--font-xl);
-  font-weight: var(--font-bold);
-`;
-
-const LoginText = styled.span`
-  color: var(--sub-font--color);
-  font-size: var(--font-md);
-  line-height: 33px;
-`;
-const LoginMain = styled.main`
-  /* padding: 0 16px; */
-  position: relative;
-  text-align: center;
-  top: 149px;
-`;
-
-const LoginBtnBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-  margin-bottom: 25px;
-`;
-
-const Span = styled.span`
-  position: relative;
-  display: block;
-  color: var(--font-color);
-  font-weight: 300;
-`;
-
-const NavUserInfo = styled.nav`
-  margin-top: 16px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const NavUserInfoLink = styled.span`
-  font-size: var(--font-sm);
-  cursor: pointer;
-`;
--- src/pages/Login/LoginStlye.tsx
+++ src/pages/Login/LoginStlye.tsx
@@ -1,1 +1,70 @@
 import styled from 'styled-components';
+
+const LoginWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 16px;
+`;
+
+const LoginHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  top: 56px;
+
+  line-height: 33px;
+`;
+
+const LoginTitle = styled.h1`
+  font-size: var(--font-xl);
+  font-weight: var(--font-bold);
+`;
+
+const LoginText = styled.span`
+  color: var(--sub-font--color);
+  font-size: var(--font-md);
+  line-height: 33px;
+`;
+const LoginMain = styled.main`
+  position: relative;
+  text-align: center;
+  top: 149px;
+`;
+
+const LoginBtnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  margin-bottom: 25px;
+`;
+
+const Span = styled.span`
+  position: relative;
+  display: block;
+  color: var(--font-color);
+  font-weight: 300;
+`;
+
+const NavUserInfo = styled.nav`
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const NavUserInfoLink = styled.span`
+  font-size: var(--font-sm);
+  cursor: pointer;
+`;
+export {
+  LoginWrap,
+  LoginHeader,
+  LoginTitle,
+  LoginText,
+  LoginMain,
+  LoginBtnBox,
+  Span,
+  NavUserInfo,
+  NavUserInfoLink,
+};
